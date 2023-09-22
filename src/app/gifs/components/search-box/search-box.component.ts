9f460828e4dd7c014a82b6db87e2231b49b3ebe7
AndersonDavid <andesondavid17@gmail.com>
2023-09-22 17:25:56 -0500

    Primer commit

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Busca Gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
+  `,
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public taginput!: ElementRef<HTMLInputElement>;
+  constructor(private gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.taginput.nativeElement.value;
+    // console.log(this.gifsService.tagsHistory);
+    this.gifsService.searchTag(newTag);
+    this.taginput.nativeElement.value = '';
+  }
+
+  public resetTag(value: string): void {
+    this.taginput.nativeElement.value = value;
+  }
+}
