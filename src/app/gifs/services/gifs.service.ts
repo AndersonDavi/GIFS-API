481b5c1d4888fa9d2280ccc4aa7a1523dcd90d5f
AndersonDavid <andesondavid17@gmail.com>
2023-09-28 17:31:19 -0500

    feat(front): Se añade historial en local storage y carga automatica la ultima busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,7 +9,9 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private GIPHY_API_KEY: string = 'vhd96GnWgloxaqF0slM4b6stu0XVaCXF';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -43,5 +45,19 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
 
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 }
