6389142eb9262fe370b78599b7dbb990cee01398
AndersonDavid <andesondavid17@gmail.com>
2023-10-02 17:23:50 -0500

    Fin seccion 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GifsModule, SharedModule,HttpClientModule],
+  imports: [BrowserModule, GifsModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
