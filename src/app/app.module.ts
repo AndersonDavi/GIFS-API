654a5e8ffdf88003433167eb772b53ab3085ae82
AndersonDavi <andesondavid17@gmail.com>
2024-06-04 15:26:55 -0500

    build: update to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    GifsModule,
-    SharedModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        GifsModule,
+        SharedModule,
+        BrowserAnimationsModule,
+        MaterialModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
